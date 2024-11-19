ff811ef299b0d3091a9cb90614b409055f2aa6a7
Raghav Thakur <you@example.com>
2024-11-19 13:58:58 +0530

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import { Register } from './components/Register';
 
 function App() {
     return (
-        <AuthProvider>
             <Router>
                 <div className="App">
                     <Navbar />
@@ -17,15 +16,12 @@ function App() {
                         <Routes>
                             <Route path="/login" element={<Login />} />
                             <Route path="/register" element={<Register />} />
-                            <Route path="/admin" element={<AdminDashboard />} />
-                            <Route path="/user" element={<User Dashboard />} />
                             <Route path="/" element={<h2>Welcome to My App</h2>} />
                         </Routes>
                     </div>
                     <Footer />
                 </div>
             </Router>
-        </AuthProvider>
     );
 }
 
